10ca4d9444e591070e904cff0bdbfad7ba18f3a9
Sumit <sumit.18.paul@gmail.com>
2021-04-07 18:22:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,12 +6,29 @@ from sklearn.calibration import CalibratedClassifierCV
 from sklearn.svm import LinearSVC
 import numpy as np
 
+import spacy
+import re
+from spacy.matcher import PhraseMatcher
+nlp = spacy.load("en_core_web_sm")
+file1 = open('static/bad_words.txt')
+file1.seek(0, 0)
+bad_list = []
+for bw in file1.readlines():
+  bad_list.append(bw.rstrip('\n'))
+
+bad_words_patterns = [nlp(text) for text in bad_list]
+matcher = PhraseMatcher(nlp.vocab)
+matcher.add('BAD_WORDS', None, *bad_words_patterns)
+
+
 model = joblib.load("trained_model/bestmodel.joblib")
 vectorizer = joblib.load("trained_model/vectorizer.joblib")
+
+
 label = ["Neutral", "Abusive"]
+
 app = Flask(__name__)
 
-res = ""
 @app.route("/")
 def home():
     return render_template('index.html')
@@ -26,8 +43,20 @@ def prediction():
     y_probability = np.max(proba)*100
     final_res = str(y_label)+":"+ str(y_probability)
     print(final_res)
-    return render_template('prediction.html', prediction = final_res)
+    return render_template('prediction.html', prediction=final_res)
 
+@app.route("/censor", methods=["POST", "GET"])
+def censor():
+    data = request.args.get("data")
+    doc = nlp(data)
+    matches = matcher(doc)
+    text = data
+    for match_id, start, end in matches:
+        span = doc[start: end]
+        len_of_word = len(span.text)
+        censor_ = "*" * len_of_word
+        text = re.sub(span.text, censor_, text)
+    return render_template('censor.html', censor_=text)
 
 
 if __name__ == '__main__':
